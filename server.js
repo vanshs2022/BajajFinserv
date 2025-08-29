2f69b5a6527a938380a21a72e85bff28bb4a9b68
Vansh Sharma <vns444555@gmail.com>
2025-08-29 10:40:24 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,78 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const FULL_NAME = "john_doe"; 
+const DOB = "17091999";       
+const EMAIL = "john@xyz.com"; 
+const ROLL_NUMBER = "ABCD123"; 
+
+function isNumeric(str) {
+  return /^\d+$/.test(str);
+}
+
+function alternatingCaps(str) {
+  let result = "";
+  let upper = true;
+  for (let ch of str) {
+    result += upper ? ch.toUpperCase() : ch.toLowerCase();
+    upper = !upper;
+  }
+  return result;
+}
+
+app.post("/bfhl", (req, res) => {
+  try {
+    const data = req.body.data;
+    if (!Array.isArray(data)) {
+      return res.status(400).json({ is_success: false, message: "Invalid input" });
+    }
+
+    let odd_numbers = [];
+    let even_numbers = [];
+    let alphabets = [];
+    let special_characters = [];
+    let sum = 0;
+
+    data.forEach(item => {
+      if (isNumeric(item)) {
+        let num = parseInt(item, 10);
+        if (num % 2 === 0) {
+          even_numbers.push(item);
+        } else {
+          odd_numbers.push(item);
+        }
+        sum += num;
+      } else if (/^[a-zA-Z]+$/.test(item)) {
+        alphabets.push(item.toUpperCase());
+      } else {
+        special_characters.push(item);
+      }
+    });
+
+    const concat_input = alphabets.join("");
+    const concat_string = alternatingCaps(concat_input.split("").reverse().join(""));
+
+    const response = {
+      is_success: true,
+      user_id: `${FULL_NAME}_${DOB}`,
+      email: EMAIL,
+      roll_number: ROLL_NUMBER,
+      odd_numbers,
+      even_numbers,
+      alphabets,
+      special_characters,
+      sum: sum.toString(),
+      concat_string
+    };
+
+    return res.status(200).json(response);
+  } catch (err) {
+    return res.status(500).json({ is_success: false, message: err.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
